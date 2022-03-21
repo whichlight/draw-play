a7ca449608f1b63cbf899917086f1f3b2b532e5e
whichlight <whichlight@gmail.com>
2022-03-20 22:47:44 -0400

    updating logic

--- colorshifting.js
+++ colorshifting.js
@@ -2,6 +2,7 @@ let w, h;
 let pathStarted = false;
 let g_path;
 let paths = [];
+let g_changeNode = [-1,-1]; 
 
 function setup() {
     w = windowWidth;
@@ -29,14 +30,12 @@ function draw() {
             let p = createVector(mouseX, mouseY);
             let p_i = g_path.points[g_path.points.length - 1];
             let d = p5.Vector.dist(p, p_i);
-            if (d > collission_dist) {
+            if (d > (collission_dist)) {
                 noStroke();
-                let c = collideColor(p, collission_dist);
-                console.log(c);
-                strokeWeight(g_path.r*0.66);
-                stroke(g_path.c, 100, 100);
+                collideColor(p_i, g_path.r);
+                strokeWeight(g_path.r);
+                stroke(g_path.c, 100, 100,0.8);
                 line(p_i.x, p_i.y, p.x, p.y);
-
                 fill(g_path.c, 100, 100);
                 noStroke();
                 ellipse(p.x,p.y,collission_dist, collission_dist);
@@ -57,7 +56,7 @@ function draw() {
 class Path {
     constructor() {
         this.points = [];
-        this.r = 15;
+        this.r = 20;
         this.c = 180; 
     }
 
@@ -78,12 +77,14 @@ class Path {
 function collideColor(p, dist) {
     //iterate through points, see if it intersects
     let c = 0; 
-    paths.forEach((path) => {
-        path.points.forEach((point) => {
+    paths.forEach((path, path_i) => {
+        path.points.forEach((point, point_i) => {
             let d = p5.Vector.dist(point, p);
-            if (d < dist) {
+            //make sure it doesn't change color twice in the same point (or neighbors) when crossing
+            if (d!=0 && d < dist && !(g_changeNode[0]==path_i && abs(g_changeNode[1]-point_i)<2)) {
                 g_path.c = newColor();
-           
+                g_changeNode = [path_i, point_i];
+                console.log(g_changeNode);
             }
         });
     });
