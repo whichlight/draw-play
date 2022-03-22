52dfc7c003d87675e0d20dc21b00d6fb91b7e22c
whichlight <whichlight@gmail.com>
2022-03-22 01:37:08 -0400

    adding colorpaths

---
+++ colorpaths.js
@@ -0,0 +1,132 @@
+
+let w, h;
+let pathStarted = false;
+let g_path;
+let paths = [];
+
+function setup() {
+    w = windowWidth;
+    h = windowHeight;
+    createCanvas(w, h);
+    colorMode(HSB, 360, 100, 100);
+    background(0, 0, 0);
+
+}
+
+function draw() {
+    //background(0, 0, 0, 0.05);
+    if (mouseIsPressed) {
+        if (!pathStarted) {
+            g_path = new Path();
+            pathStarted = true;
+            paths.push(g_path);
+
+        }
+        if (pathStarted) {
+            g_path.addPoint(mouseX, mouseY);
+        }
+    }
+
+    if (!mouseIsPressed) {
+        pathStarted = false;
+    }
+
+    paths.forEach((p) => {
+        p.drawPath();
+    });
+
+
+    paths.forEach((p) => {
+        p.highlight();
+        p.collides();
+        p.update();
+    });
+
+
+}
+
+class Point {
+    constructor(p, c) {
+        this.pos = p;
+        this.x = this.pos.x;
+        this.y = this.pos.y;
+        this.c = c;
+    }
+
+}
+
+class Path {
+    constructor() {
+        this.points = [];
+        this.index = 0;
+        this.r = 20;
+        this.c = 180;
+        this.glow = 100;
+        this.changeIndex = -1;
+    }
+
+    addPoint(x, y) {
+        let p = new Point(createVector(x, y), this.c);
+        this.points.push(p);
+    }
+
+    drawPath() {
+        this.points.forEach((p, i) => {
+            if (i < (this.points.length - 1)) {
+                strokeWeight(this.r);
+                stroke(p.c, this.glow, 100);
+                line(p.x, p.y, this.points[i + 1].x, this.points[i + 1].y);
+
+                if (i == this.index) {
+                    p.c = this.c;
+                }
+            }
+        })
+    }
+
+    highlight() {
+        let p = this.points[this.index];
+        noStroke();
+        fill(p.c, 0, 100);
+        ellipse(p.x, p.y, this.r, this.r);
+    }
+
+    update() {
+        if (this.glow < 100) this.glow += 10;
+        this.index++;
+        this.index %= this.points.length;
+        if (this.index == 0) this.changeIndex = -1;
+    }
+
+    collides() {
+        paths.forEach((path) => {
+            let p2 = path.points[path.index];
+            let p1 = this.points[this.index];
+            let d = p5.Vector.dist(p1.pos, p2.pos);
+
+            if (d < this.r / 2 && path != this) {
+
+                //check there wasn't a collission nearby
+                let farEnough = false;
+                if (this.changeIndex > -1) {
+                    let p_coll = this.points[this.changeIndex];
+                    farEnough = p5.Vector.dist(p1.pos, p_coll.pos) > this.r;
+                }
+
+                if (farEnough || this.changeIndex == -1) {
+                    this.colorUpdate();
+                    this.changeIndex = this.index;
+                }
+            }
+        });
+    }
+
+    colorUpdate() {
+        this.c += 60;
+        this.c %= 360;
+    }
+
+
+
+
+}
